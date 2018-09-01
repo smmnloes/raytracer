bbf93ae96dcc8336641657364cefcc0f3771a782
maximilian <m.loesch@hotmail.de>
2018-09-01 20:26:41 +0200

    allow width < height

--- src/engine/Raytracer.java
+++ src/engine/Raytracer.java
@@ -8,7 +8,7 @@ import engine.util.Vector3D;
 
 public class Raytracer {
     private static final int IMAGE_HEIGHT = 500;
-    private static final int IMAGE_WIDTH = 500;
+    private static final int IMAGE_WIDTH = 1000;
 
     private final SceneData sceneData;
 
@@ -23,12 +23,23 @@ public class Raytracer {
         int[][] imageBuffer = new int[IMAGE_HEIGHT][IMAGE_WIDTH];
 
         for (int y = 0; y < IMAGE_HEIGHT; y++) {
-            for (int x = 0; x < IMAGE_HEIGHT; x++) {
+            for (int x = 0; x < IMAGE_WIDTH; x++) {
                 double fov = sceneData.camera.fov;
-                double imageAspectRatio = IMAGE_WIDTH / IMAGE_HEIGHT; // assuming width > height
                 double scale = Math.tan(fov / 2 * Math.PI / 180);
-                double Px = (2 * ((x + 0.5) / IMAGE_WIDTH) - 1) * scale * imageAspectRatio;
-                double Py = (1 - 2 * ((y + 0.5) / IMAGE_HEIGHT) * scale);
+
+
+                double Px, Py;
+
+                if (IMAGE_WIDTH > IMAGE_HEIGHT) {
+                    double imageAspectRatio = (double) IMAGE_WIDTH / (double) IMAGE_HEIGHT; // assuming width > height
+                    Px = (2 * ((x + 0.5) / IMAGE_WIDTH) - 1) * scale * imageAspectRatio;
+                    Py = (1 - 2 * ((y + 0.5) / IMAGE_HEIGHT) * scale);
+                } else {
+                    double imageAspectRatio = (double) IMAGE_HEIGHT / (double) IMAGE_WIDTH; // assuming width < height
+                    Px = (2 * ((x + 0.5) / IMAGE_WIDTH) - 1) * scale;
+                    Py = (1 - 2 * ((y + 0.5) / IMAGE_HEIGHT) * scale * imageAspectRatio);
+                }
+
                 // TODO: add support for camera movement
                 Vector3D rayOrigin = new Vector3D(0, 0, 0);
                 Vector3D rayDirection = Vector3D.minus(new Vector3D(Px, Py, -1), rayOrigin); // note that this just equal to Vec3f(Px, Py, -1);
