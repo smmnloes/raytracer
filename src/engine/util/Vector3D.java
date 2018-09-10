819e799ee4511e148648919f358c5d61be1fb100
maximilian <m.loesch@hotmail.de>
2018-09-10 15:31:43 +0200

    add transforms to vector3d class

--- src/engine/util/Vector3D.java
+++ src/engine/util/Vector3D.java
@@ -1,5 +1,7 @@
 package engine.util;
 
+import static java.lang.Math.*;
+
 public class Vector3D {
     public double x, y, z;
 
@@ -7,6 +9,10 @@ public class Vector3D {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public Vector3D(double[] asArray) {
+        this(asArray[0], asArray[1], asArray[2]);
     }
 
     public Vector3D minus(Vector3D b) {
@@ -26,7 +32,7 @@ public class Vector3D {
     }
 
     public double length() {
-        return Math.sqrt((x*x) + (y*y) + (z*z));
+        return sqrt((x * x) + (y * y) + (z * z));
     }
 
     public double dotProduct(Vector3D b) {
@@ -38,6 +44,90 @@ public class Vector3D {
     }
 
     public Vector3D negate() {
-        return new Vector3D(-x, -y, - z);
+        return new Vector3D(-x, -y, -z);
     }
+
+    public Vector3D translate(Vector3D b) {
+        return plus(b);
+    }
+
+    public Vector3D rotate(Vector3D rotation){
+        return rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z);
+    }
+
+    public Vector3D scale(Vector3D factor) {
+        double[][] scaleMatrix = new double[][]{
+                {factor.x, 0, 0, 0},
+                {0, factor.y, 0, 0},
+                {0, 0, factor.z, 0, 0},
+                {0, 0, 0, 1}
+        };
+        return matrixMult(scaleMatrix);
+    }
+
+    public Vector3D transform(Vector3D translation, Vector3D rotation, Vector3D scale) {
+        return translate(translation).rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z).scale(scale);
+    }
+
+    private Vector3D rotateX(double rotationX) {
+        double[][] rotationXMatrix = new double[][]
+                {
+                        {1, 0, 0, 0},
+                        {0, cos(rotationX), -sin(rotationX), 0},
+                        {0, sin(rotationX), cos(rotationX), 0},
+                        {0, 0, 0, 1}
+                };
+        return matrixMult(rotationXMatrix);
+    }
+
+    private Vector3D rotateY(double rotationY) {
+        double[][] rotationYMatrix = new double[][]
+                {
+                        {cos(rotationY), 0, sin(rotationY), 0},
+                        {0, 1, 0, 0},
+                        {-sin(rotationY), 0, cos(rotationY), 0},
+                        {0, 0, 0, 1}
+                };
+        return matrixMult(rotationYMatrix);
+    }
+
+    private Vector3D rotateZ(double rotationZ) {
+        double[][] rotationZMatrix = new double[][]
+                {
+                        {cos(rotationZ), -sin(rotationZ), 0, 0},
+                        {sin(rotationZ), cos(rotationZ), 0, 0},
+                        {0, 0, 1, 0},
+                        {0, 0, 0, 1}
+                };
+        return matrixMult(rotationZMatrix);
+    }
+
+
+    private Vector3D matrixMult(double[][] matrix) {
+        RuntimeException not4x4quadratic = new RuntimeException("Matrix must be 4x4 and quadratic");
+        if (matrix.length != 4) {
+            throw not4x4quadratic;
+        }
+
+        for (double[] line : matrix) {
+            if (line.length != 4) {
+                throw not4x4quadratic;
+            }
+        }
+
+        double[] result = new double[4];
+        double[] thisExtended = new double[]{x, y, z, 1};
+
+        for (int i = 0; i < 4; i++) {
+            int resultRow = 0;
+            for (int j = 0; j < 4; j++) {
+                resultRow += matrix[i][j] * thisExtended[j];
+            }
+            result[i] = resultRow;
+        }
+
+        return new Vector3D(result);
+
+    }
+
 }
